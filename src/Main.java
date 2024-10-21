def086e326d4e2f456851c5ff17d6abf4836a931
user.name <deanrakan@webmail.UMM.ac.id>
2024-10-21 09:57:31 +0700

    mahall

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukan nilai a =");
+        int a = input.nextInt();
+        System.out.print("masukan nilai b =");
+        int b = input.nextInt();
+        int c = a+b;
+        System.out.print("nilai c adalah =" + c);
+
+
+
+    }
+}
